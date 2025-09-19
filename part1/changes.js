3dce803e1907fdfc5d647b8c74b02581364140b1
omdhar-bit <omdharb@gmail.com>
2025-09-20 00:12:07 +0530

    ghgf

---
+++ part1/changes.js
@@ -0,0 +1,14 @@
+let gameName = "Cricket"
+
+gameName = "Badminton"
+
+console.log(gameName)
+
+const username = "OMdhar"
+
+username = "Om"
+
+console.log(username)
+
+// let = value can be changes
+//  and const means it cant be changed
