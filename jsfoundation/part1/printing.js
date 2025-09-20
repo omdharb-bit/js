7ba6c84478241bd3380d63961b45a94749c7291d
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-09-20 12:18:17 +0530

    Add files via upload

---
+++ jsfoundation/part1/printing.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+process.stdout.write("Om");
+
+console.table({city: "Jabalpur"})
+console.warn({city: "Jabalpur"})
---
+++ jsfoundation/part2/nonPrimitive.js
@@ -0,0 +1,24 @@
+const obj = {
+  firstname: "Om",
+  isLoggedin: true,
+}
+
+
+// obj.firstname = "Mr. Om"
+obj.lastname = "Dhar"
+
+// console.log(obj.firstname);
+// console.log(obj.lastname);
+// console.log(obj);
+// console.log(typeof obj)
+
+let today = new Date()
+// console.log(today.getDate())
+
+//Array
+
+let heros = ["a", "b", "c"]
+// console.log(heros[0])
+
+let isValue = true
+console.log(isValue + 1)
