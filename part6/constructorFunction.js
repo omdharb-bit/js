db83e3d9add55a0e8ff8570ac2c8459edf7f8b2d
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-09-24 15:41:48 +0530

    bfg

---
+++ part6/constructorFunction.js
@@ -0,0 +1,21 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Car(make, model) {
+this.make = make;
+this.model = model;
+}
+
+let myCar = new Car("Toyota", "Corolla");
+// console.log(myCar);
+
+function Tea(type) {
+  this.type = type;
+  this.describe = function () {
+    return `This is a cup of ${this.type} tea.`;
+  };
+}
+let lemonTea = new Tea("lemon tea");
+console.log(lemonTea.describe()); 
