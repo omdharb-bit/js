88b77c2adfd0583a5ae95b10d841b4059de3bb09
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-09-24 15:46:02 +0530

    ghgh

--- part6/constructorFunction.js
+++ part6/constructorFunction.js
@@ -18,4 +18,14 @@ function Tea(type) {
   };
 }
 let lemonTea = new Tea("lemon tea");
-console.log(lemonTea.describe()); 
+// console.log(lemonTea.describe()); 
+
+function Animal(species) {
+  this.species = species;
+}
+Animal.prototype.sound = function () {
+  return `${this.species} makes a sound.`;
+
+}
+let dog = new Animal("Dog");
+console.log(dog.sound());
