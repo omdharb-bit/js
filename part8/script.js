2e89ee8ca0c95c6973db0b53ed1d4ebeef666627
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-09-26 13:30:56 +0530

    g

--- part8/script.js
+++ part8/script.js
@@ -50,3 +50,51 @@ taskList.lastElementChild.remove()
 })
 
 
+// example 6
+
+document.getElementById("clickMeButton").addEventListener("click", function () {
+ alert("chaicode")
+})
+
+// example 7
+
+
+document.getElementById("teaList").addEventListener("click", function (event) {
+  if (event.target && event.target.matches('.teaItem')) {
+  alert("You selected: "+ event.target.textContent)
+  
+  }
+
+})
+
+
+// example 8
+
+
+document.getElementById("feedbackform").addEventListener('submit', function (event) {
+  event.preventDefault()
+  let feedback=document.getElementById("feedbackInput").value
+console.log(feedback);
+  document.getElementById("feedbackDisplay").textContent = `Feedback is: ${feedback}`;
+
+})
+
+
+// example 9
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM fully loaded";
+
+
+})
+
+
+// example 10
+
+
+document.getElementById("toggleHighlight").addEventListener("click", function () {
+
+let descriptionText= document.getElementById("descriptionText")
+  descriptionText.classList.add("highlight");
+})
